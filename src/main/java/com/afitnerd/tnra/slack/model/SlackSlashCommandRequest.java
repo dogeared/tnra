854e7cdf7670dc6bc50d29c98aef2c36ece0ad1e
Micah Silverman <micah@afitnerd.com>
2020-08-15 11:07:08 -0400

    updated to ignore unknown properties

--- src/main/java/com/afitnerd/tnra/slack/model/SlackSlashCommandRequest.java
+++ src/main/java/com/afitnerd/tnra/slack/model/SlackSlashCommandRequest.java
@@ -1,7 +1,9 @@
 package com.afitnerd.tnra.slack.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SlackSlashCommandRequest {
 
     private String token;
