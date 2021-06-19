70f3cffd20b444ad0c9e6145adbb0a302576f1f0
Micah Silverman <micah@afitnerd.com>
2021-06-19 18:34:04 -0400

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java $JAVA_OPTS -Dserver.port=$PORT -Dlogging.level.com.zaxxer.hikari=DEBUG -Dspring.profiles.active=prod -jar target/*.jar
+web:    java $JAVA_OPTS -Dserver.port=$PORT -Dspring.profiles.active=prod -jar target/*.jar
--- src/main/java/com/afitnerd/tnra/controller/HomeController.java
+++ src/main/java/com/afitnerd/tnra/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/login/callback", "/"})
+    @RequestMapping(value = {"/login/callback"})
     public String redirect() {
         return "forward:/";
     }
