5940e54f9cf94d4723cde0b5f090934b46f5f6b2
Micah Silverman <micah@afitnerd.com>
2021-11-25 22:50:47 -0800

    changed initial auth state for Stats tab

--- src/frontend/src/components/Stats.vue
+++ src/frontend/src/components/Stats.vue
@@ -66,7 +66,7 @@ export default {
   data() {
     return {
       stats: [],
-      isAuthenticated: false,
+      isAuthenticated: true,
       authError: false,
       pqEmail: '',
       pqPassword: ''
